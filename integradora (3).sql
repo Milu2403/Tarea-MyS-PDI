eadb7d2a332167804e2b99b0027bcb18f74f5eb3
Milu2403 <100946848+Milu2403@users.noreply.github.com>
2022-11-17 15:18:12 -0300

    Add files via upload

---
+++ integradora (3).sql
@@ -0,0 +1,108 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.3
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 11-11-2022 a las 15:25:16
+-- Versión del servidor: 10.4.24-MariaDB
+-- Versión de PHP: 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `integradora`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `datos`
+--
+
+CREATE TABLE `datos` (
+  `legajo` int(11) NOT NULL,
+  `apellido` varchar(25) NOT NULL,
+  `nombre` varchar(25) NOT NULL,
+  `fecha_de_ingreso` datetime DEFAULT NULL,
+  `puesto` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `datos`
+--
+
+INSERT INTO `datos` (`legajo`, `apellido`, `nombre`, `fecha_de_ingreso`, `puesto`) VALUES
+(1, '', '', '0000-00-00 00:00:00', 2),
+(2, 'Montaño', 'Luciano', '2004-06-11 00:00:00', 1),
+(3, 'Montaño', 'Luciano', '2004-06-11 00:00:00', 1),
+(4, 'Montaño', 'Luciano', '2004-06-11 00:00:00', 1),
+(5, 'Villalba', 'Milagros', '2022-03-03 00:00:00', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `puestos`
+--
+
+CREATE TABLE `puestos` (
+  `id` int(11) NOT NULL,
+  `sueldos` int(6) NOT NULL,
+  `puesto` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `puestos`
+--
+
+INSERT INTO `puestos` (`id`, `sueldos`, `puesto`) VALUES
+(1, 100000, 'director'),
+(2, 90000, 'Profesor'),
+(3, 50000, 'Preseptor'),
+(4, 550000, 'Secretario'),
+(5, 70000, 'representante'),
+(6, 90000, 'inspector');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `datos`
+--
+ALTER TABLE `datos`
+  ADD PRIMARY KEY (`legajo`);
+
+--
+-- Indices de la tabla `puestos`
+--
+ALTER TABLE `puestos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `datos`
+--
+ALTER TABLE `datos`
+  MODIFY `legajo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `puestos`
+--
+ALTER TABLE `puestos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
